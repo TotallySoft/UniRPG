703547c055b4396daa8b606176a79bc4970db80e
TotallySoft <reuben@totallysoft.ch>
2010-11-20 19:18:32 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UniRPG::Application.config.secret_token = '8ee01672b831db75fc6638c5ebff9c1d938e85b30ba70593c218333476433da2c66992d583d216b9fd87dab608cc9d28d453fb96534d3d781cadb21076a4ae97'
